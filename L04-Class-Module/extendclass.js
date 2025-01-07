a15026ac04fcf568f23340b8f2d5c821c585b05b
BlackBloodKCZ <147476701+WarinyuphaJY@users.noreply.github.com>
2025-01-07 09:50:30 +0700

    Update extendclass.js

--- L04-Class-Module/extendclass.js
+++ L04-Class-Module/extendclass.js
@@ -13,3 +13,11 @@ class Song extends Media {
         this.artist = songData.artist;
     }
 }
+
+const mySong = new Song({
+    artist: "Queen",
+    name: "Bohemian Rhapsody",
+    publishDate: 1975,
+});
+
+console.log(mySong);
